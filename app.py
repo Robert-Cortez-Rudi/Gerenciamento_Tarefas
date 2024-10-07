1307a08b179e76915d03b84d1e4b346b44cede4c
Robert Rudi <robertcortezrudi@gmail.com>
2024-10-07 13:57:41 -0300

    Correção no app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager 
+
 
 app = Flask(__name__)
 
@@ -7,7 +9,7 @@ app = Flask(__name__)
 def principal():
     return render_template("index.html")
 
-
+# TERMINAR
 
 
 if __name__ == "__main__":
