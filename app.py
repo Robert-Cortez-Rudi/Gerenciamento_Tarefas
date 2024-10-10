06f90cc9f7311d8b94da3444b98ac85ef85d70e1
Robert Rudi <robertcortezrudi@gmail.com>
2024-10-10 16:44:32 -0300

    Alteração no arquivo principal do app

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager 
+from gerenciador_tarefas import create_app
 
-
-app = Flask(__name__)
-
-@app.route("/")
-def principal():
-    return render_template("index.html")
-
-# TERMINAR
-
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
