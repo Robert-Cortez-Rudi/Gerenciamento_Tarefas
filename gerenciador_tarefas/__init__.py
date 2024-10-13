c56804ea8d909525321d73aecbabf8cff26a466f
Robert Rudi <robertcortezrudi@gmail.com>
2024-10-12 23:35:37 -0300

    __init__.py do sistema

--- gerenciador_tarefas/__init__.py
+++ gerenciador_tarefas/__init__.py
@@ -2,24 +2,34 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config, DevelopmentConfig, ProductionConfig
-from routes import usuario
 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+
 def create_app():
     app = Flask(__name__)
+    print("App criado")
     app.config.from_object(DevelopmentConfig) # Pode ser alterado para modo de desenvolvimento ou de produção
 
     db.init_app(app)
+    print("DB inicializado")
 
-    with app.app_context(): # Insere contexto ao app para a criação das tabelas do banco de dados
-        db.create_all() # Criação das tabelas no banco de dados
 
     login_manager.init_app(app)
+    print("Login manager inicializado")
+
     login_manager.login_view = "auth.login"
 
+    with app.app_context():
+        from .models import User, Task
+
+        db.create_all()
+        print("Tabelas criadas")
+
+
+    from .routes import usuario
     app.register_blueprint(usuario)
 
     return app
