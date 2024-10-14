b853167168244014a622c10ccd38e746f227f038
Robert Rudi <robertcortezrudi@gmail.com>
2024-10-13 23:42:38 -0300

    Formulário de criação e edição de tarefas

---
+++ gerenciador_tarefas/forms/task_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class CreateTaskForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(min=1, max=255)])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    due_date = DateField("Due date", format="%d-%m-%Y" , validators=[DataRequired()])
+    submit = SubmitField("Criar Tarefa")
+
+
+class EditTaskForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(min=1, max=255)])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    due_date = DateField("Due date", format="%Y-%m-%d" , validators=[DataRequired()])
+    submit = SubmitField("Salvar Alterações")
