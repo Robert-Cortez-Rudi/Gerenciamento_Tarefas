2145f0c28a7b531e1d1959acf1a9bb3a50c81e7f
Robert Rudi <robertcortezrudi@gmail.com>
2024-10-19 23:05:22 -0300

    Atualização do formulário de cadastro e mensagens

--- gerenciador_tarefas/forms/register_form.py
+++ gerenciador_tarefas/forms/register_form.py
@@ -3,15 +3,25 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    paswword = PasswordField(
-        "Password",
+    username = StringField("Username", validators=[DataRequired(message="O nome de usuário é obrigatório")])
+    email = StringField(
+        "Email", 
         validators=[
-            DataRequired(),
-            Length(min=6),
-            EqualTo("paswword_confirm", message="As senhas não coincidem")
+            DataRequired(message="O email é obrigatório!"), 
+            Email(message="Forneça um email válido!")
         ]
     )
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired()])
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(message="Insira a senha"),
+            Length(min=6, message="A senha deve ter no mínimo 6 digitos"),
+        ]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password", 
+        validators=[DataRequired(message="A confirmação da senha é obrigatória"), 
+                    EqualTo("confirm_password", message="As senhas não coincidem")
+                    ]
+        )
     submit = SubmitField("Cadastrar")
