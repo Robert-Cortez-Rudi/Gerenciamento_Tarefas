2838f365d433277f27ee49a743ffbf2e0f617ae2
Robert Rudi <robertcortezrudi@gmail.com>
2024-10-27 18:46:49 -0300

    Atribuição de exclusão de tarefas junto com a conta

--- gerenciador_tarefas/models/user.py
+++ gerenciador_tarefas/models/user.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
 
-    tasks = db.relationship("Task", backref="user")
+    tasks = db.relationship("Task", backref="user", cascade="all, delete-orphan")
 
     def __init__(self, username, email, password):
         self.username = username
