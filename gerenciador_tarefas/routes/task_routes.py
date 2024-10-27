678559bde417a7c18504aa2fd293fca797d4ff70
Robert Rudi <robertcortezrudi@gmail.com>
2024-10-27 18:49:20 -0300

    Ajustes na lógica nas rotas de dashboard, criação, edição e exclusão de tarefas

--- gerenciador_tarefas/routes/task_routes.py
+++ gerenciador_tarefas/routes/task_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from gerenciador_tarefas.models.tasks import Task
 from gerenciador_tarefas import db
-from datetime import datetime
+from datetime import datetime, date
 from gerenciador_tarefas.forms import CreateTaskForm, EditTaskForm
 
 tasks = Blueprint("tasks", __name__)
@@ -11,22 +11,17 @@ tasks = Blueprint("tasks", __name__)
 @tasks.route("/dashboard")
 @login_required
 def dashboard():
-    user_tasks = Task.query.filter_by(id=current_user.id).all()
-    return render_template("dashboard.html", tasks=user_tasks, title="Suas Tarefas")
+    user_tasks = Task.query.filter_by(user_id=current_user.id).all()
+    current_date = datetime.today().date()
+    return render_template("dashboard.html", tasks=user_tasks, current_date=current_date)
 
 
 @tasks.route("/dashboard/new_task", methods=["GET", "POST"])
 @login_required
 def create_task():
+    form = CreateTaskForm()
     if request.method == "POST":
-
-        form = CreateTaskForm()
         if form.validate_on_submit():
-            try:
-                form.due_date.data = datetime.strptime("%d-%m-%Y")
-            except ValueError:
-                flash("Data invalida! Use o formato DD-MM-YYYY.", "danger")
-                return redirect(url_for("tasks.create_task"))
 
             new_task = Task(
                 title=form.title.data,
@@ -36,19 +31,15 @@ def create_task():
             ) 
 
             db.session.add(new_task)
-            db.commit()
-            flash("Tarefa criada com sucesso!", "success")
+            db.session.commit()
             return redirect(url_for("tasks.dashboard"))
-    return render_template("task_form.html")
+    return render_template("task_form.html", form=form)
 
 
 @tasks.route("/dashboard/<int:id>/delete/")
 @login_required
 def delete_task(id):
     task = Task.query.get_or_404(id)
-    if task != current_user.id:
-        flash("Você não tem permissão para deletar essa tarefa!", "danger")
-        return redirect(url_for("tasks.dashboard"))
     
     db.session.delete(task)
     db.session.commit()
@@ -59,13 +50,10 @@ def delete_task(id):
 @tasks.route("/dashboard/<int:id>/edit", methods=["GET", "POST"])
 @login_required
 def edit_task(id):
+    task = Task.query.get_or_404(id)
+    
+    form = EditTaskForm(obj=task)
     if request.method == "POST":
-        task = Task.query.get_or_404(id)
-        if task != current_user.id:
-            flash("Você não tem permissão para editar essa tarefa!", "danger")
-            return redirect(url_for("tasks.dashboard"))
-        
-        form = EditTaskForm()
         if form.validate_on_submit():
             task.title = form.title.data
             task.description = form.description.data
@@ -79,7 +67,7 @@ def edit_task(id):
         db.session.commit()
         flash("A sua tarefa foi atualizada com sucesso!", "success")
         return redirect(url_for("tasks.dashboard"))
-    return render_template("edit_task.html", """form=form - ARRUMAR""", task=task)
+    return render_template("edit_task.html", form=form, task=task)
     
 
 @tasks.route("/dashboard/completed/<int:id>", methods=["GET", "POST"])
@@ -94,6 +82,6 @@ def task_completed(id):
     db.session.commit()
     
     status_message = "finalizada" if task.completed else "marcada como incompleta"
-    flash(f"Tarefa {status_message} com sucesso!", "success" if task.completed else "danger")
+    flash(f"Tarefa {status_message}!", "success" if task.completed else "danger")
     return redirect(url_for("tasks.dashboard"))
 
