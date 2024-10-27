7f4b71b765d466baeeb4e5808de85af99d250a15
Robert Rudi <robertcortezrudi@gmail.com>
2024-10-26 23:07:31 -0300

    Ajustes no formulário de tarefas

--- gerenciador_tarefas/forms/task_form.py
+++ gerenciador_tarefas/forms/task_form.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length
 class CreateTaskForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired(), Length(min=1, max=255)])
     description = TextAreaField("Description", validators=[DataRequired()])
-    due_date = DateField("Due date", format="%d-%m-%Y" , validators=[DataRequired()])
+    due_date = DateField("Due date", format="%Y-%m-%d" , validators=[DataRequired()])
     submit = SubmitField("Criar Tarefa")
 
 
